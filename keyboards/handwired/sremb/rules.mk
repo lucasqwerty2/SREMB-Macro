dab03f65f44da0fd8031c592e7a5a78100dead75
FOUNDATIONQWERTY <lucasdebski2009@gmail.com>
2026-05-24 19:04:04 -0400

    Reformat rules.mk for better readability

--- keyboards/handwired/sremb/rules.mk
+++ keyboards/handwired/sremb/rules.mk
@@ -4,15 +4,11 @@ OLED_ENABLE = yes
 OLED_DRIVER = ssd1306
 EXTRAKEY_ENABLE = yes
 
-# Try disabling LTO to resolve the linker "undefined reference" errors
 LTO_ENABLE = no
 
-# Vial core feature flags
-# Explicitly adding VIA_ENABLE ensures the dynamic keymap subsystem initializes
 VIA_ENABLE = yes
 VIAL_ENABLE = yes
 
-# Disable unused advanced features
 COMBO_ENABLE = no
 TAP_DANCE_ENABLE = no
 KEY_OVERRIDE_ENABLE = no
